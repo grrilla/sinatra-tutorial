0ea350240f18724ea47349f155622f68fc420b0c
grrilla <mike.longauer@gmail.com>
2016-08-08 16:38:54 +0000

    Add validation to new message form

--- app/actions.rb
+++ app/actions.rb
@@ -8,6 +8,24 @@ get '/messages' do
   erb :'messages/index'
 end
 
+post '/messages' do
+  @message = Message.new(
+    title: params[:title],
+    content: params[:content],
+    author: params[:author]
+  )
+  if @message.save
+    redirect '/messages'
+  else
+    erb :'messages/new'
+  end
+end
+
 get '/messages/new' do
   erb :'messages/new'
+end
+
+get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'/messages/show'
 end
