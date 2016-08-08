ec815d9e3570510c8b139c58612ad1372326546d
grrilla <mike.longauer@gmail.com>
2016-08-08 15:57:14 +0000

    adding new message form

---
+++ db/migrate/20160808131209_dd_messages.rb
@@ -0,0 +1,9 @@
+class DdMessages < ActiveRecord::Migration
+  def change
+    create_table :messages do |t|
+      t.string :title
+      t.string :author
+      t.string :content
+    end
+  end
+end
